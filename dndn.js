137ef20e0660d6b88f7068559bcaa592fb743a48
luckygamerv2 <45069336+luckygamerv2@users.noreply.github.com>
2019-02-18 15:51:14 +0300

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,9 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "-"
+
+
+
+
+
+client.login("NTQ2NjU3NjYxMTAyNDU2ODMz.D0w5HQ.56g3l9hTuu0a52ka07SDnC_8jZ0")
